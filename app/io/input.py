8e8bfebdf73e89e70a5354f3b281bd2e3c6382cf
Damian Dragovoz <damiandragovoz@gmail.com>
2024-03-19 15:40:12 +0200

    Blank functions

---
+++ app/io/input.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+def input_text_from_console():
+    pass
+
+
+def read_from_file(path: str):
+    pass
+
+
+def read_from_file_using_pandas(path: str):
+    pass
+
---
+++ app/io/output.py
@@ -0,0 +1,6 @@
+def output_console(text: str):
+    pass
+
+
+def output_file(text: str):
+    pass
